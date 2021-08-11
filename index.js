23ea3bef6eb26ee238ad5d6cb781ad36557c23a4
Garrett Yaworski <garrett_yaworski@neimanmarcus.com>
2021-08-10 22:29:47 -0500

    full CRUD

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express');
+const massive = require('massive');
+
+const { getMovies, addMovie, updateTheaterStatus, deleteMovie } = require('./movieController');
+
+const app = express();
+
+app.use(express.json());
+
+massive({
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+})
+.then((dbInstance) => {
+    app.set('db', dbInstance);
+    console.log('Database connection established successfully');
+})
+.catch((e) => {
+    console.log('DB connection problem: ', e)
+});
+
+app.get('/api/movies', getMovies);
+app.post('/api/movies', addMovie);
+app.put('/api/movies/:id', updateTheaterStatus);
+app.delete('/api/movies/:id', deleteMovie);
+
+const PORT = process.env.SERVER_PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
